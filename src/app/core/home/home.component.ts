5149a00da502f31b30d17850e09990fcc51c11aa
gianni <gianni@sinapsi.local>
2018-12-12 22:30:11 +0100

    arrow  on context menu

--- src/app/core/home/home.component.ts
+++ src/app/core/home/home.component.ts
@@ -3,7 +3,9 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styles: []
+  styles: ['.arrow-up {width: 0; eight: 0; border-left: 10px solid transparent; border-right: 10px solid transparent; border-bottom: 10px solid orange;position:absolute ;top:-13px; left:5px}',
+    '.top-border {border-top:3px solid orange; border-radius:0 0}']
+
 })
 export class HomeComponent implements OnInit {
 
