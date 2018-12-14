7ef9a29163904ac1fa7e8d4cb722882d48913500
gianni <gianni@sinapsi.local>
2018-12-14 11:54:34 +0100

    Store created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SharedModule } from './shared/shared.module';
 import { WelcomeComponent } from './features/welcome/welcome.component';
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
 
 @NgModule({
   declarations: [
@@ -19,7 +23,9 @@ import { CoreModule } from './core/core.module';
     BrowserModule,
     AppRoutingModule, BrowserAnimationsModule,
     CoreModule,
-    SharedModule
+    SharedModule,
+    StoreModule.forRoot(reducers, { metaReducers }),
+    !environment.production ? StoreDevtoolsModule.instrument() : []
 
   ],
   providers: [],
